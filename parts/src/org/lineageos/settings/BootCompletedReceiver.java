186d9138fdb2ed0ac88049104064ffd9327ac4fa
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-02-04 17:56:58 -0600

    parts: Import refresh rate menu
    * Special thanks tos @PeterCxy and @TheScarastic for picasso imp
    * using custom dtbo for timing@1
    * This commit has a strict dependency of the previous commits about OSS dtbo
    	1.- 09d5866
    	2.- 8b7b5e2
    Bugs:
    	1.- Some devices will fallback to 30Hz and requieres a new device model prop
    	2.- On some roms, the default Refresh rate at first boot is 77hz
    NOTES:
    	1.- I am not responsable if this mod causes damages in total or partial way
    	2.- I am expecting a proper authorship of the commits (Including their commit message)
    	3.- Tested on Descendant and Pixel Experience
    	4.- You need to ship dtbo.img on your ROM package, because of that you need releasetools.py
    
    Source of dtbo: https://github.com/Official-Ayrton990/android_kernel_xiaomi_sm8250/commit/050741ad518c27e7189e79ae707a87c538ae6ef3
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,10 +22,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import org.lineageos.settings.fod.FodUtils;
+import android.provider.Settings;
+import android.content.pm.PackageManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
+
+import org.lineageos.settings.fod.FodUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -34,6 +38,23 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+     try {
+            // We need to reset this setting to trigger an update in display service
+            final float refreshRate = Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 77.0f);
+            Thread.sleep(500);
+            Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 77.0f);
+            Thread.sleep(500);
+            Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, refreshRate);
+        } catch (Exception e) {
+            // Ignore
+        }
+
+
+
+    
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
